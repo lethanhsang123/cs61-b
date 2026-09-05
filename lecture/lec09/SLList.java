155835b503a747bd4e49d4d5f2c8c6f9fef86b0f
sanglt <sanglt@pvcombank.com.vn>
2026-09-05 15:59:40 +0700

    lecture 09

--- lecture/lec09/SLList.java
+++ lecture/lec09/SLList.java
@@ -34,14 +34,23 @@ public class SLList<T> implements List61B<T> {
 
     @Override
     public T getFirst() {
+        if (this.size == 0) {
+            return null;
+        }
         return this.sentinel.next.item;
     }
 
     
     @Override
     public T getLast() {
-        // Todo
-        return null;
+        if (this.size == 0) {
+            return null;
+        }
+        Node<T> node = this.sentinel;
+        while (node.next != null) {
+            node = node.next;
+        }
+        return node.item;
     }
 
     /** Add an item to the end of the list. */
@@ -63,18 +72,63 @@ public class SLList<T> implements List61B<T> {
 
     @Override
     public T removeLast() {
-        // Todo
-        return null;
+        if (this.size <= 0) {
+            return null;
+        }
+        // Jump to size - 1 node;
+        Node<T> previNode = this.sentinel;
+        while (previNode.next.next != null) {
+            previNode = previNode.next;
+        }
+
+        // remove next
+        Node<T> lastNode = previNode.next;
+        previNode.next = null;
+        
+        // decrease size
+        this.size--;
+
+        return lastNode.item;
     }
 
     @Override
     public T get(int i) {
-        // Todo
-        return null;
+        if (i < 0 || i >= this.size) {
+            return null;
+        }
+        Node<T> node = this.sentinel;
+        while (i >= 0) {
+            node = node.next;
+            i--;
+        }
+        return node.item;
     }
 
     @Override
     public void insert(T x, int position) {
-        // Todo
+        // validate
+        if (position < 0 || position > size) {
+            return;
+        }
+
+        // check add last
+        if (position == size) {
+            addLast(x);
+            return;
+        }
+
+        // jump to previous node
+        Node<T> previous = this.sentinel;
+        while (position > 0) {
+            previous = previous.next;
+            position --;
+        }
+
+        // add new node
+        Node<T> newNode = new Node<T>(x, previous.next);
+        previous.next = newNode;
+
+        // increase size
+        this.size ++;
     }
 }
